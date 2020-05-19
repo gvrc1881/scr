09302a25b4fce8d33666006d0ef0aaede87de571
ADIREDDY <HARSHA@DESKTOP-FV7OF12>
2020-05-19 15:57:56 +0530

    sequence defined for failure analysis

--- src/main/java/com/scr/model/FailureAnalysis.java
+++ src/main/java/com/scr/model/FailureAnalysis.java
@@ -6,6 +6,8 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -15,6 +17,7 @@ public class FailureAnalysis implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
 	@Column(name = "failure_id")
