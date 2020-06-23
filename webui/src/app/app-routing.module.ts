e1039b3b7b2a6ba9eee9c65e73903b1ef6ebb56f
opentaps <opentaps@ZONE-SYS4>
2020-06-23 15:02:05 +0530

    model module by bhavani

--- webui/src/app/app-routing.module.ts
+++ webui/src/app/app-routing.module.ts
@@ -287,7 +287,11 @@ const routes: Routes = [
     loadChildren: './components/energy-bill-payment/make/make.module#MakeModule',
     canActivate: [AuthGuard]
   },
-
+  {
+    path: 'model',
+    loadChildren: './components/energy-bill-payment/model/model.module#ModelModule',
+    canActivate: [AuthGuard]
+  }
 ];
 
 @NgModule({
---
+++ webui/src/app/models/model.model.ts
@@ -0,0 +1,9 @@
+export interface ModelModel{
+    brandName: string ,
+    createdBy: string, 
+    description:string,
+    modelCode :string,
+    modelName :string,
+    modelType :string
+      
+  }
