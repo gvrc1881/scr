6d05ad5fde0ecfa53558b8f4f2a8e9ca237bce28
Adi Reddy <team.wr@winfocus.co.in>
2020-11-21 10:12:55 +0530

    build issue

---
+++ src/main/java/com/scr/message/response/WPASectionTargetsResponse.java
@@ -0,0 +1,174 @@
+package com.scr.message.response;
+
+import org.springframework.stereotype.Component;
+
+import com.scr.model.WorkGroup;
+import com.scr.model.WorkPhaseActivity;
+
+@Component
+public class WPASectionTargetsResponse {
+
+	private Integer id;
+	private WorkPhaseActivity workPhaseActivityId;
+	private WorkGroup workGroupId;
+	private String yearType;
+	private Integer year;
+	private Double apr;
+	private Double may;
+	private Double jun;;
+	private Double jul;
+	private Double aug;
+	private Double sep;
+	private Double oct;
+	private Double nov;
+	private Double dec;
+	private Double jan;
+	private Double feb;
+	private Double mar;
+	private String population;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public WorkPhaseActivity getWorkPhaseActivityId() {
+		return workPhaseActivityId;
+	}
+
+	public void setWorkPhaseActivityId(WorkPhaseActivity workPhaseActivityId) {
+		this.workPhaseActivityId = workPhaseActivityId;
+	}
+
+	public WorkGroup getWorkGroupId() {
+		return workGroupId;
+	}
+
+	public void setWorkGroupId(WorkGroup workGroupId) {
+		this.workGroupId = workGroupId;
+	}
+
+	public String getYearType() {
+		return yearType;
+	}
+
+	public void setYearType(String yearType) {
+		this.yearType = yearType;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public Double getApr() {
+		return apr;
+	}
+
+	public void setApr(Double apr) {
+		this.apr = apr;
+	}
+
+	public Double getMay() {
+		return may;
+	}
+
+	public void setMay(Double may) {
+		this.may = may;
+	}
+
+	public Double getJun() {
+		return jun;
+	}
+
+	public void setJun(Double jun) {
+		this.jun = jun;
+	}
+
+	public Double getJul() {
+		return jul;
+	}
+
+	public void setJul(Double jul) {
+		this.jul = jul;
+	}
+
+	public Double getAug() {
+		return aug;
+	}
+
+	public void setAug(Double aug) {
+		this.aug = aug;
+	}
+
+	public Double getSep() {
+		return sep;
+	}
+
+	public void setSep(Double sep) {
+		this.sep = sep;
+	}
+
+	public Double getOct() {
+		return oct;
+	}
+
+	public void setOct(Double oct) {
+		this.oct = oct;
+	}
+
+	public Double getNov() {
+		return nov;
+	}
+
+	public void setNov(Double nov) {
+		this.nov = nov;
+	}
+
+	public Double getDec() {
+		return dec;
+	}
+
+	public void setDec(Double dec) {
+		this.dec = dec;
+	}
+
+	public Double getJan() {
+		return jan;
+	}
+
+	public void setJan(Double jan) {
+		this.jan = jan;
+	}
+
+	public Double getFeb() {
+		return feb;
+	}
+
+	public void setFeb(Double feb) {
+		this.feb = feb;
+	}
+
+	public Double getMar() {
+		return mar;
+	}
+
+	public void setMar(Double mar) {
+		this.mar = mar;
+	}
+
+	public String getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(String population) {
+		this.population = population;
+	}
+
+}
