effac6a8c1cbd66fce77e1f526a330e74a3ab69b
winfocus <winfocus@RDP-SRV1>
2021-01-27 13:03:44 +0530

    committed by gopal after modify,from date to date reports

--- webui/src/environments/environment.prod.ts
+++ webui/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://scrtrd.herokuapp.com/scr/api',
-  stipulationUrl:'D:/SCR/',
-  inspectionUrl:'D:/SCR/'
+  //apiUrl: 'http://122.175.41.15:9090/scr/api',
+  apiUrl: 'http://localhost:9090/scr/api',
+  stipulationUrl:'/home/Opentaps/git/',
+  inspectionUrl:'/home/Opentaps/git/'
 };
